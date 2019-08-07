5481c8d502e4fca7f412bf5994ab8ef113ab7fd6
Mick 277 <fadboyfiction@yandex.com>
2019-08-07 21:51:48 +1000

    changes to git attributes

--- vagrant-up.cmd
+++ vagrant-up.cmd
@@ -3,13 +3,18 @@
 @SET SCRIPTNAME=%~nx0
 @SET CURRENTDIR=%~dp0
 
-@FOR /F "tokens=5" %%G IN ('vagrant global-status^|find /I "poweroff"') DO @(
-	cd %%G
-	vagrant up
+@REM vagrant global-status
+
+@FOR /F "tokens=2" %%G IN ('vagrant global-status^|find /I "aborted"') DO @(
+	SET V_NAME=%%G
+	IF NOT "%V_NAME%"=="" ECHO %CURRENTDIR%%V_NAME%
+
 )
-@FOR /F "tokens=5" %%G IN ('vagrant global-status^|find /I "aborted"') DO @(
-	cd %%G
-	vagrant up
+
+@FOR /F "tokens=2" %%G IN ('vagrant global-status^|find /I "poweroff"') DO @(
+	SET V_NAME=%%G
+	IF NOT "%V_NAME%"=="" ECHO %CURRENTDIR%%V_NAME%
 )
-vagrant global-status
+
+@REM vagrant global-status
 
