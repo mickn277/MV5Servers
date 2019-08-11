e07afa367355421480eded21a4c3d14a3ba75470
Mick 277 <fadboyfiction@yandex.com>
2019-08-11 22:39:56 +1000

    minor change

--- Vagrant-OracleApex19c-XE18c/provision-scripts/config-user-env.sh
+++ Vagrant-OracleApex19c-XE18c/provision-scripts/config-user-env.sh
@@ -3,12 +3,12 @@
 echo 'CONFIG ENV: Start region settings'
 
 #MW: Use rlwrap for sqlplus
-yum install -y rlwrap
 echo 'CONFIG ENV: Install rlwrap'
+yum install -y rlwrap
 
+echo 'CONFIG ENV: add alias rlwrap for sqlplus rman'
 echo '# add rlwrap' >> /home/oracle/.bashrc
 echo 'alias sqlplus="rlwrap -i $ORACLE_HOME/bin/sqlplus"' >> /home/oracle/.bashrc
 echo 'alias rman="rlwrap -i $ORACLE_HOME/bin/rman"' >> /home/oracle/.bashrc
-echo 'CONFIG ENV: add alias rlwrap for sqlplus rman'
 
 echo 'CONFIG ENV: Complete region settings'
